59c6251e79fba2b30904b9f3b8f1fb2e8bccc5c1
John <jrmtaylor@gmail.com>
2023-09-07 15:55:09 -0400

    Create giver.js

---
+++ JobPts/giver.js
@@ -0,0 +1,69 @@
+function getRecognitions(){
+  const appDomain = window.location.host;
+  let totalPages = 0;
+  let currentPage = 0;
+  const listHeaderUrl = 'https://' + appDomain + '/services/GetListOfRecognitionsForUser.xsjs?type=giver';
+  const listUrl = 'https://' + appDomain + '/services/GetListOfRecognitionsForUser.xsjs?type=giver&pageParam=###&SearchParam=';
+  const recognitionUrl = 'https://' + appDomain + '/services/GetRecognitionDetails.xsjs?type=giver&id=###'
+  let recognitionList = [];
+  let allRecognitions = [];
+  
+  const xhr1 = new XMLHttpRequest();
+  xhr1.open("GET", listHeaderUrl, false);
+  xhr1.send();
+  if(xhr1.status == 200){
+    totalPages = JSON.parse(xhr1.responseText).pagination.TotalPages;
+  } else {
+    console.error("xhr1 status " + xhr1.status + "!!!");
+  }
+  
+  console.info("retrieving list of recognitions");
+  while(currentPage < totalPages)
+  {
+    currentPage++;
+    const xhr2 = new XMLHttpRequest();
+    xhr2.open("GET", listUrl.replace('###', currentPage), false);
+    xhr2.send();
+    if(xhr2.status == 200){
+      recognitionList = recognitionList.concat(JSON.parse(xhr2.responseText).result)
+    } else {
+      console.error("xhr2 status " + xhr2.status + "!!!");				
+    }
+  }
+  
+  console.info("retrieving recognitions");
+  recognitionList.forEach(
+    function(arrayItem) {
+      allRecognitions.push(getRecognition(recognitionUrl.replace('###', arrayItem.Id)))
+      switch(allRecognitions.length) { 
+        case Math.round(recognitionList.length/4):
+          console.info("25% complete");
+          break;
+        case Math.round(recognitionList.length/2):
+          console.info("50% complete");
+          break;
+        case Math.round(3*recognitionList.length/4):
+          console.info("75% complete");
+          break;
+        }
+    }
+  );
+  return allRecognitions;
+}
+
+function getRecognition(url) {
+  const tempRecognition = {RecognizeMessage: "", FullNameReceiver : "", TransactionDate: ""}; 
+  const xhr = new XMLHttpRequest();
+  xhr.open("GET", url, false);
+  xhr.send();
+  if(xhr.status == 200){
+    let recognition = JSON.parse(xhr.responseText);
+    tempRecognition.RecognizeMessage = recognition.RecognizeMessage;
+    tempRecognition.FullNameReceiver = recognition.FullNameReceiver;
+    tempRecognition.TransactionDate = recognition.TransactionDate;
+    return tempRecognition;
+  } else {
+    console.error("xhr status " + xhr.status + "!!!");
+  }
+}
+getRecognitions()
